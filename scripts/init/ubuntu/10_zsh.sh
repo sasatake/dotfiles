f3984f0934c0f7e9e599e418541363ce6df8efa3
sasatake <t06418as@gmail.com>
2021-10-28 12:03:23 +0900

    rollback zplug install method

--- scripts/init/ubuntu/10_zsh.sh
+++ scripts/init/ubuntu/10_zsh.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-readonly ZPLUG_INSTALL_URL=https://raw.githubusercontent.com/zplug/installer/master/installer.zsh
-
-
 zsh_is_not_installed(){
   ! type zsh > /dev/null
-}
-
-zplug_is_not_installed(){
-  ! type zplug > /dev/null
 }
 
 install_zsh(){
   info "start installing z shell..."
   yes | sudo apt install zsh && clear "zsh is installed by apt"
-}
-
-install_zplug(){
-  info "start installing zplug..."
-  curl -sL --proto-redir -all,https ${ZPLUG_INSTALL_URL} | zsh && clear "zplug is installed"
 }
 
 zsh_is_not_login_shell(){
@@ -34,5 +22,4 @@ set_zsh_to_login_shell(){
 
 sub_title "Check Shell"
 zsh_is_not_installed && install_zsh || clear "zsh path is $(which zsh)"
-zplug_is_not_installed && install_zplug && clear "zplug is installed"
 zsh_is_not_login_shell && set_zsh_to_login_shell || clear "$(zsh --version)"
